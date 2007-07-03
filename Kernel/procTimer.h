b6bc48eada378767b907de5e176580f6bb160c7f
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2007-07-03 14:38:10 +0000

    Initialise timer variable to make -Wall happy
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@66 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/procTimer.h
+++ Kernel/procTimer.h
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2006 by Dmitry Tsarkov
+Copyright (C) 2003-2007 by Dmitry Tsarkov
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -45,7 +45,7 @@ private:	// methods
 
 public:		// interface
 		/// the only c'tor
-	TsProcTimer ( void ) { Reset(); }
+	TsProcTimer ( void ) : startTime(0) { Reset(); }
 		/// empty d'tor
 	~TsProcTimer ( void ) {}
 
