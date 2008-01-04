e4e1afff650e6d09dc90c31d1880b3a466dcc490
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2008-01-04 16:47:05 +0000

    Increase max identifier size for LISP reasoner
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@183 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- FaCT++/scanner.h
+++ FaCT++/scanner.h
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2007 by Dmitry Tsarkov
+Copyright (C) 2003-2008 by Dmitry Tsarkov
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -31,7 +31,7 @@ Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include "grammar.h"
 
 /// max ID length for scanned objects
-const unsigned int MaxIDLength = 256;
+const unsigned int MaxIDLength = 1024;
 
 /// more-or-less general simple scanner implementation
 class TsScanner
