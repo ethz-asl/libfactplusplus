8e74fbcec018c79c95a816bfb4c6fc8a90990c23
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2008-01-09 21:06:18 +0000

    Generalise axiom absorption cycle a bit
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@193 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/tAxiomSet.cpp
+++ Kernel/tAxiomSet.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2007 by Dmitry Tsarkov
+Copyright (C) 2003-2008 by Dmitry Tsarkov
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -63,7 +63,7 @@ bool TAxiomSet :: absorbGCI ( TAxiom* p )
 	{
 		// steps 2-3. Simplify and unfold
 		if ( absorbSimplifyFirst )
-			if ( p->simplify() )
+			if ( simplify(p) )
 				continue;
 
 		// R-or-C part a): necessary
@@ -86,7 +86,7 @@ bool TAxiomSet :: absorbGCI ( TAxiom* p )
 
 		// steps 2-3. Simplify and unfold
 		if ( !absorbSimplifyFirst )
-			if ( p->simplify() )
+			if ( simplify(p) )
 				continue;
 
 		// R-or-C part c): late
--- Kernel/tAxiomSet.h
+++ Kernel/tAxiomSet.h
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2007 by Dmitry Tsarkov
+Copyright (C) 2003-2008 by Dmitry Tsarkov
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -102,6 +102,8 @@ protected:	// methods
 
 		return ret;
 	}
+		/// simplify given axiom.
+	bool simplify ( TAxiom* p ) { return p->simplify(); }
 
 	// absorption statistics
 
