1612908676617a6e3bab4d8e07b4e5f71b4aac33
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2008-10-07 21:00:53 +0000

    Update changed yesr
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@372 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- DIGParser/DIGimplOutput.cpp
+++ DIGParser/DIGimplOutput.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2007 by Dmitry Tsarkov
+Copyright (C) 2003-2008 by Dmitry Tsarkov
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
