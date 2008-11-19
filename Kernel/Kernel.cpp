c7e936961285bccee88a49385273fdfd12b4dba8
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2008-11-19 19:51:51 +0000

    Release 1.2.1
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@429 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -19,12 +19,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "Kernel.h"
 #include "comerror.h"
 
-const char* ReasoningKernel :: Version = "1.2.0";
+const char* ReasoningKernel :: Version = "1.2.1";
 const char* ReasoningKernel :: ProductName =
 	"FaCT++.Kernel: Reasoner for the SROIQ(D) Description Logic\n";
 const char* ReasoningKernel :: Copyright =
 	"Copyright (C) Dmitry V. Tsarkov, 2002-2008. ";
-const char* ReasoningKernel :: ReleaseDate = "(25 September 2008)";
+const char* ReasoningKernel :: ReleaseDate = "(20 November 2008)";
 
 // print the FaCT++ information only once
 static bool KernelFirstRun = true;
