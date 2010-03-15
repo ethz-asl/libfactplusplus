4bec1a5bb5509db2787c7f8f137c334f1d663606
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2010-03-15 15:48:21 +0000

    Add #define to switch between OWLAPI 2/3
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@718 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- FaCT++.JNI/FaCTPlusPlus.cpp
+++ FaCT++.JNI/FaCTPlusPlus.cpp
@@ -18,6 +18,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <sstream>
 
+// use OWL API v3 settings
+//#define OWLAPI3
+
 // switch tracing on
 //#define JNI_TRACING
 
