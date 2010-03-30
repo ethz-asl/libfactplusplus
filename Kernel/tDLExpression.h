78141aad13d5c27761f4fc3801cf921954b54b58
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2010-03-30 18:39:51 +0000

    Add general expressions
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@729 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/tDLExpression.h
+++ Kernel/tDLExpression.h
@@ -25,10 +25,30 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 // forward declaration for all expression classes: necessary for the visitor pattern
 class TDLExpression;
 
+class TDLConceptExpression;
+
+class TDLRoleExpression;
+class TDLObjectRoleExpression;
+
+class TDLDataRoleExpression;
+
+class TDLIndividualExpression;
+
+class TDLDataExpression;
+
 /// general visitor for DL expressions
 class DLExpressionVisitor
 {
 public:		// visitor interface
+	// concept expressions
+
+	// object role expressions
+
+	// data role expressions
+
+	// data expressions
+
+	// other methods
 	virtual ~DLExpressionVisitor ( void ) {}
 }; // DLExpressionVisitor
 
@@ -45,5 +65,111 @@ public:		// interface
 		/// accept method for the visitor pattern
 	virtual void accept ( DLExpressionVisitor& visitor ) = 0;
 }; // TDLExpression
+
+
+//------------------------------------------------------------------
+//	concept expressions
+//------------------------------------------------------------------
+
+
+//------------------------------------------------------------------
+///	general concept expression
+//------------------------------------------------------------------
+class TDLConceptExpression: public TDLExpression
+{
+public:		// interface
+		/// empty c'tor
+	TDLConceptExpression ( void ) : TDLExpression() {}
+		/// empty d'tor
+	virtual ~TDLConceptExpression ( void ) {}
+
+		/// accept method for the visitor pattern
+	void accept ( DLExpressionVisitor& visitor ) = 0;
+}; // TDLConceptExpression
+
+
+//------------------------------------------------------------------
+//	role expressions
+//------------------------------------------------------------------
+
+
+//------------------------------------------------------------------
+///	general role expression
+//------------------------------------------------------------------
+class TDLRoleExpression: public TDLExpression
+{
+public:		// interface
+		/// empty c'tor
+	TDLRoleExpression ( void ) : TDLExpression() {}
+		/// empty d'tor
+	virtual ~TDLRoleExpression ( void ) {}
+
+		/// accept method for the visitor pattern
+	void accept ( DLExpressionVisitor& visitor ) = 0;
+}; // TDLRoleExpression
+
+
+//------------------------------------------------------------------
+//	object role expressions
+//------------------------------------------------------------------
+
+
+//------------------------------------------------------------------
+///	general object role expression
+//------------------------------------------------------------------
+class TDLObjectRoleExpression: public TDLRoleExpression
+{
+public:		// interface
+		/// empty c'tor
+	TDLObjectRoleExpression ( void ) : TDLRoleExpression() {}
+		/// empty d'tor
+	virtual ~TDLObjectRoleExpression ( void ) {}
+
+		/// accept method for the visitor pattern
+	void accept ( DLExpressionVisitor& visitor ) = 0;
+}; // TDLObjectRoleExpression
+
+
+//------------------------------------------------------------------
+//	data role expressions
+//------------------------------------------------------------------
+
+
+//------------------------------------------------------------------
+///	general data role expression
+//------------------------------------------------------------------
+class TDLDataRoleExpression: public TDLRoleExpression
+{
+public:		// interface
+		/// empty c'tor
+	TDLDataRoleExpression ( void ) : TDLRoleExpression() {}
+		/// empty d'tor
+	virtual ~TDLDataRoleExpression ( void ) {}
+
+		/// accept method for the visitor pattern
+	void accept ( DLExpressionVisitor& visitor ) = 0;
+}; // TDLDataRoleExpression
+
+
+//------------------------------------------------------------------
+//	data expressions
+//------------------------------------------------------------------
+
+
+//------------------------------------------------------------------
+///	general data expression
+//------------------------------------------------------------------
+class TDLDataExpression: public TDLExpression
+{
+public:		// interface
+		/// empty c'tor
+	TDLDataExpression ( void ) : TDLExpression() {}
+		/// empty d'tor
+	virtual ~TDLDataExpression ( void ) {}
+
+		/// accept method for the visitor pattern
+	void accept ( DLExpressionVisitor& visitor ) = 0;
+}; // TDLDataExpression
+
 
 #endif
