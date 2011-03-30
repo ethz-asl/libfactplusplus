28971faf1f08c49d18fb7a93d186f0ef81eb2645
rafl@debian.org <rafl@debian.org@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2011-03-30 16:18:32 +0000

    Include string.h for strcmp
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@1134 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/taxVertex.h
+++ Kernel/taxVertex.h
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <set>
 #include <vector>
 #include <iostream>
+#include <string.h>
 
 #include "taxNamEntry.h"
 #include "tLabeller.h"
