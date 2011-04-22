12d52cc73fef41dee2e183f8c7c251bb7384985f
rafl@debian.org <rafl@debian.org@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2011-04-22 22:35:03 +0000

    Use C comments for the C header file
    There's plenty of C compilers out there that either don't like C++ comments at
    all, warn about them, or reject them in some strict mode or another. Let's try
    to be compatible.
    
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@1152 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- FaCT++.C/fact.h
+++ FaCT++.C/fact.h
@@ -5,38 +5,38 @@
 extern "C" {
 #endif
 
-// type declarations
+/* type declarations */
 
 #define DECLARE_STRUCT(name) typedef struct name ## _st name
-// FaCT++ kernel
+/* FaCT++ kernel */
 DECLARE_STRUCT(fact_reasoning_kernel);
-// progress monitor
+/* progress monitor */
 DECLARE_STRUCT(fact_progress_monitor);
-// axiom
+/* axiom */
 DECLARE_STRUCT(fact_axiom);
-// expression
+/* expression */
 DECLARE_STRUCT(fact_expression);
-// concept expression
+/* concept expression */
 DECLARE_STRUCT(fact_concept_expression);
-// data- or object-role expression
+/* data- or object-role expression */
 DECLARE_STRUCT(fact_role_expression);
-// object role expression
+/* object role expression */
 DECLARE_STRUCT(fact_o_role_expression);
-// complex object role expression
+/* complex object role expression */
 DECLARE_STRUCT(fact_o_role_complex_expression);
-// data role expression
+/* data role expression */
 DECLARE_STRUCT(fact_d_role_expression);
-// individual expression
+/* individual expression */
 DECLARE_STRUCT(fact_individual_expression);
-// general data expression
+/* general data expression */
 DECLARE_STRUCT(fact_data_expression);
-// data type expression
+/* data type expression */
 DECLARE_STRUCT(fact_data_type_expression);
-// data value expression
+/* data value expression */
 DECLARE_STRUCT(fact_data_value_expression);
-// facet expression
+/* facet expression */
 DECLARE_STRUCT(fact_facet_expression);
-// actor to traverse taxonomy
+/* actor to traverse taxonomy */
 DECLARE_STRUCT(fact_actor);
 
 #undef DECLARE_STRUCT
@@ -46,7 +46,7 @@ const char *fact_get_version ();
 fact_reasoning_kernel *fact_reasoning_kernel_new (void);
 void fact_reasoning_kernel_free (fact_reasoning_kernel *);
 
-/*
+/* * */
 ifOptionSet* getOptions (  );
 const ifOptionSet* getOptions (  );
  */
@@ -222,7 +222,7 @@ bool fact_is_same_individuals (fact_reasoning_kernel *,
 bool fact_is_instance (fact_reasoning_kernel *,
 		fact_individual_expression *i,
 		fact_concept_expression *c);
-/*
+/* * */
 void fact_get_related_roles (fact_reasoning_kernel *,
 			     fact_individual_expression *i,
 			     bool data, bool needI,
@@ -242,139 +242,139 @@ fact_actor* fact_individual_actor_new();
 fact_actor* fact_o_role_actor_new();
 fact_actor* fact_d_role_actor_new();
 void fact_actor_free(fact_actor*);
-/// get 1-d NULL-terminated array of synonyms of the 1st entry(necessary for Equivalents, for example)
+/* get 1-d NULL-terminated array of synonyms of the 1st entry(necessary for Equivalents, for example) */
 const char** fact_get_synonyms ( fact_actor* );
-/// get NULL-terminated 2D array of all required elements of the taxonomy
+/* get NULL-terminated 2D array of all required elements of the taxonomy */
 const char*** fact_get_elements_2d ( fact_actor* );
-/// get NULL-terminated 1D array of all required elements of the taxonomy
+/* get NULL-terminated 1D array of all required elements of the taxonomy */
 const char** fact_get_elements_1d ( fact_actor* );
 
-/// opens new argument list
+/* opens new argument list */
 void fact_new_arg_list ( fact_reasoning_kernel *k );
-/// add argument _a_rG to the current argument list
+/* add argument _a_rG to the current argument list */
 void fact_add_arg ( fact_reasoning_kernel *k,fact_expression* e );
 
-// create expressions methods
+/* create expressions methods */
 
-// concepts
+/* concepts */
 
-/// get _t_o_p concept
+/* get _t_o_p concept */
 fact_concept_expression* fact_top ( fact_reasoning_kernel *k );
-/// get _b_o_t_t_o_m concept
+/* get _b_o_t_t_o_m concept */
 fact_concept_expression* fact_bottom ( fact_reasoning_kernel *k );
-/// get named concept
+/* get named concept */
 fact_concept_expression* fact_concept ( fact_reasoning_kernel *k,const char* name );
-/// get negation of a concept c
+/* get negation of a concept c */
 fact_concept_expression* fact_not ( fact_reasoning_kernel *k,fact_concept_expression* c );
-/// get an n-ary conjunction expression; take the arguments from the last argument list
+/* get an n-ary conjunction expression; take the arguments from the last argument list */
 fact_concept_expression* fact_and ( fact_reasoning_kernel *k );
-/// get an n-ary disjunction expression; take the arguments from the last argument list
+/* get an n-ary disjunction expression; take the arguments from the last argument list */
 fact_concept_expression* fact_or ( fact_reasoning_kernel *k );
-/// get an n-ary one-of expression; take the arguments from the last argument list
+/* get an n-ary one-of expression; take the arguments from the last argument list */
 fact_concept_expression* fact_one_of ( fact_reasoning_kernel *k );
 
-/// get self-reference restriction of an object role r
+/* get self-reference restriction of an object role r */
 fact_concept_expression* fact_self_reference ( fact_reasoning_kernel *k,fact_o_role_expression* r );
-/// get value restriction wrt an object role r and an individual i
+/* get value restriction wrt an object role r and an individual i */
 fact_concept_expression* fact_o_value ( fact_reasoning_kernel *k,fact_o_role_expression* r, fact_individual_expression* i );
-/// get existential restriction wrt an object role r and a concept c
+/* get existential restriction wrt an object role r and a concept c */
 fact_concept_expression* fact_o_exists ( fact_reasoning_kernel *k,fact_o_role_expression* r, fact_concept_expression* c );
-/// get universal restriction wrt an object role r and a concept c
+/* get universal restriction wrt an object role r and a concept c */
 fact_concept_expression* fact_o_forall ( fact_reasoning_kernel *k,fact_o_role_expression* r, fact_concept_expression* c );
-/// get min cardinality restriction wrt number _n, an object role r and a concept c
+/* get min cardinality restriction wrt number _n, an object role r and a concept c */
 fact_concept_expression* fact_o_min_cardinality ( fact_reasoning_kernel *k,unsigned int n, fact_o_role_expression* r, fact_concept_expression* c );
-/// get max cardinality restriction wrt number _n, an object role r and a concept c
+/* get max cardinality restriction wrt number _n, an object role r and a concept c */
 fact_concept_expression* fact_o_max_cardinality ( fact_reasoning_kernel *k,unsigned int n, fact_o_role_expression* r, fact_concept_expression* c );
-/// get exact cardinality restriction wrt number _n, an object role r and a concept c
+/* get exact cardinality restriction wrt number _n, an object role r and a concept c */
 fact_concept_expression* fact_o_cardinality ( fact_reasoning_kernel *k,unsigned int n, fact_o_role_expression* r, fact_concept_expression* c );
 
-/// get value restriction wrt a data role r and a data value v
+/* get value restriction wrt a data role r and a data value v */
 fact_concept_expression* fact_d_value ( fact_reasoning_kernel *k,fact_d_role_expression* r, fact_data_value_expression* v );
-/// get existential restriction wrt a data role r and a data expression e
+/* get existential restriction wrt a data role r and a data expression e */
 fact_concept_expression* fact_d_exists ( fact_reasoning_kernel *k,fact_d_role_expression* r, fact_data_expression* e );
-/// get universal restriction wrt a data role r and a data expression e
+/* get universal restriction wrt a data role r and a data expression e */
 fact_concept_expression* fact_d_forall ( fact_reasoning_kernel *k,fact_d_role_expression* r, fact_data_expression* e );
-/// get min cardinality restriction wrt number _n, a data role r and a data expression e
+/* get min cardinality restriction wrt number _n, a data role r and a data expression e */
 fact_concept_expression* fact_d_min_cardinality ( fact_reasoning_kernel *k,unsigned int n, fact_d_role_expression* r, fact_data_expression* e );
-/// get max cardinality restriction wrt number _n, a data role r and a data expression e
+/* get max cardinality restriction wrt number _n, a data role r and a data expression e */
 fact_concept_expression* fact_d_max_cardinality ( fact_reasoning_kernel *k,unsigned int n, fact_d_role_expression* r, fact_data_expression* e );
-/// get exact cardinality restriction wrt number _n, a data role r and a data expression e
+/* get exact cardinality restriction wrt number _n, a data role r and a data expression e */
 fact_concept_expression* fact_d_cardinality ( fact_reasoning_kernel *k,unsigned int n, fact_d_role_expression* r, fact_data_expression* e );
 
-// individuals
+/* individuals */
 
-/// get named individual
+/* get named individual */
 fact_individual_expression* fact_individual ( fact_reasoning_kernel *k,const char* name );
 
-// object roles
+/* object roles */
 
-/// get _t_o_p object role
+/* get _t_o_p object role */
 fact_o_role_expression* fact_object_role_top ( fact_reasoning_kernel *k );
-/// get _b_o_t_t_o_m object role
+/* get _b_o_t_t_o_m object role */
 fact_o_role_expression* fact_object_role_bottom ( fact_reasoning_kernel *k );
-/// get named object role
+/* get named object role */
 fact_o_role_expression* fact_object_role ( fact_reasoning_kernel *k,const char* name );
-/// get an inverse of a given object role expression r
+/* get an inverse of a given object role expression r */
 fact_o_role_expression* fact_inverse ( fact_reasoning_kernel *k,fact_o_role_expression* r );
-/// get a role chain corresponding to _r1 o ... o _rn; take the arguments from the last argument list
+/* get a role chain corresponding to _r1 o ... o _rn; take the arguments from the last argument list */
 fact_o_role_complex_expression* fact_compose ( fact_reasoning_kernel *k );
-/// get a expression corresponding to r projected from c
+/* get a expression corresponding to r projected from c */
 fact_o_role_complex_expression* fact_project_from ( fact_reasoning_kernel *k,fact_o_role_expression* r, fact_concept_expression* c );
-/// get a expression corresponding to r projected into c
+/* get a expression corresponding to r projected into c */
 fact_o_role_complex_expression* fact_project_into ( fact_reasoning_kernel *k,fact_o_role_expression* r, fact_concept_expression* c );
 
-// data roles
+/* data roles */
 
-/// get _t_o_p data role
+/* get _t_o_p data role */
 fact_d_role_expression* fact_data_role_top ( fact_reasoning_kernel *k );
-/// get _b_o_t_t_o_m data role
+/* get _b_o_t_t_o_m data role */
 fact_d_role_expression* fact_data_role_bottom ( fact_reasoning_kernel *k );
-/// get named data role
+/* get named data role */
 fact_d_role_expression* fact_data_role ( fact_reasoning_kernel *k,const char* name );
 
-// data expressions
+/* data expressions */
 
-/// get _t_o_p data element
+/* get _t_o_p data element */
 fact_data_expression* fact_data_top ( fact_reasoning_kernel *k );
-/// get _b_o_t_t_o_m data element
+/* get _b_o_t_t_o_m data element */
 fact_data_expression* fact_data_bottom ( fact_reasoning_kernel *k );
 
-/// get named data type
+/* get named data type */
 fact_data_type_expression* fact_data_type ( fact_reasoning_kernel *k,const char* name );
-/// get basic string data type
+/* get basic string data type */
 fact_data_type_expression* fact_get_str_data_type ( fact_reasoning_kernel *k );
-/// get basic integer data type
+/* get basic integer data type */
 fact_data_type_expression* fact_get_int_data_type ( fact_reasoning_kernel *k );
-/// get basic floating point data type
+/* get basic floating point data type */
 fact_data_type_expression* fact_get_real_data_type ( fact_reasoning_kernel *k );
-/// get basic boolean data type
+/* get basic boolean data type */
 fact_data_type_expression* fact_get_bool_data_type ( fact_reasoning_kernel *k );
-/// get basic date-time data type
+/* get basic date-time data type */
 fact_data_type_expression* fact_get_time_data_type ( fact_reasoning_kernel *k );
 
-/// get basic boolean data type
+/* get basic boolean data type */
 fact_data_type_expression* fact_restricted_type ( fact_reasoning_kernel *k,fact_data_type_expression* type, fact_facet_expression* facet );
 
-/// get data value with given _v_a_lU_e and _tY_p_e;
-// _f_iX_m_e!! now change the type to the basic type of the given one
-// _that is, value of a type positive_integer will be of a type _integer
+/* get data value with given _v_a_lU_e and _tY_p_e; */
+/* _f_iX_m_e!! now change the type to the basic type of the given one */
+/* _that is, value of a type positive_integer will be of a type _integer */
 fact_data_value_expression* fact_data_value ( fact_reasoning_kernel *k,const char* value, fact_data_type_expression* type );
-/// get negation of a data expression e
+/* get negation of a data expression e */
 fact_data_expression* fact_data_not ( fact_reasoning_kernel *k,fact_data_expression* e );
-/// get an n-ary data conjunction expression; take the arguments from the last argument list
+/* get an n-ary data conjunction expression; take the arguments from the last argument list */
 fact_data_expression* fact_data_and ( fact_reasoning_kernel *k );
-/// get an n-ary data disjunction expression; take the arguments from the last argument list
+/* get an n-ary data disjunction expression; take the arguments from the last argument list */
 fact_data_expression* fact_data_or ( fact_reasoning_kernel *k );
-/// get an n-ary data one-of expression; take the arguments from the last argument list
+/* get an n-ary data one-of expression; take the arguments from the last argument list */
 fact_data_expression* fact_data_one_of ( fact_reasoning_kernel *k );
 
-/// get min_inclusive facet with a given _v
+/* get min_inclusive facet with a given _v */
 fact_facet_expression* fact_facet_min_inclusive ( fact_reasoning_kernel *k,fact_data_value_expression* v );
-/// get min_exclusive facet with a given _v
+/* get min_exclusive facet with a given _v */
 fact_facet_expression* fact_facet_min_exclusive ( fact_reasoning_kernel *k,fact_data_value_expression* v );
-/// get max_inclusive facet with a given _v
+/* get max_inclusive facet with a given _v */
 fact_facet_expression* fact_facet_max_inclusive ( fact_reasoning_kernel *k,fact_data_value_expression* v );
-/// get max_exclusive facet with a given _v
+/* get max_exclusive facet with a given _v */
 fact_facet_expression* fact_facet_max_exclusive ( fact_reasoning_kernel *k,fact_data_value_expression* v );
 
 #ifdef __cplusplus
