4ac30ad4934a690c10ed9f90e31b314abf4a2b80
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2012-02-24 15:03:44 +0000

    Remove debug usage of dtCollection
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@1342 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/Tactic.cpp
+++ Kernel/Tactic.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2011 by Dmitry Tsarkov
+Copyright (C) 2003-2012 by Dmitry Tsarkov
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -258,7 +258,7 @@ bool DlSatTester :: commonTacticBodySingleton ( const DLVertex& cur )
 bool DlSatTester :: commonTacticBodyAnd ( const DLVertex& cur )
 {
 #ifdef ENABLE_CHECKING
-	fpp_assert ( isPositive(curConcept.bp()) && ( cur.Type() == dtAnd || cur.Type() == dtCollection ) );	// safety check
+	fpp_assert ( isPositive(curConcept.bp()) && ( cur.Type() == dtAnd ) );	// safety check
 #endif
 
 	incStat(nAndCalls);
