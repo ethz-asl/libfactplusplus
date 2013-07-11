e4e89c883bacdd0b4f54271c7ba4424501ae8112
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2013-07-11 16:00:50 +0000

    Separate extended data range code from Kernel.cpp
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@1590 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -23,8 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "AtomicDecomposer.h"
 #include "OntologyBasedModularizer.h"
 #include "eFPPSaveLoad.h"
-#include "dlCompletionTree.h"	// for extended data ranges
-#include "DataReasoning.h"		// for extended data ranges
 
 const char* ReasoningKernel :: Version = "1.6.2";
 const char* ReasoningKernel :: SupportedDL = "SROIQ(D)";
@@ -593,88 +591,6 @@ ReasoningKernel :: isRelated ( const TIndividualExpr* I, const TORoleExpr* R, co
 			return true;
 
 	return false;
-}
-
-static bool
-checkDataRelation ( const DataTypeReasoner& Op1, const DataTypeReasoner& Op2, int op )
-{
-	switch (op)
-	{
-	case 0:	// =
-		return Op1 == Op2;
-	case 1:	// !=
-		return !(Op1 == Op2);
-	case 2:	// <
-		return Op1 < Op2;
-	case 3: // <=
-		return Op1 < Op2 || Op1 == Op2;
-	case 4:	// >
-		return Op2 < Op1;
-	case 5: // >=
-		return Op2 < Op1 || Op2 == Op1;
-	default:
-		throw new EFaCTPlusPlus("Illegal operation in checkIndividualValues()");
-	}
-	return false;
-}
-
-static bool
-fillDTReasoner ( DataTypeReasoner& DTR, const DlCompletionTree* node )
-{
-	DTR.clear();
-	// data node may contain only "simple" concepts in there
-	for ( DlCompletionTree::const_label_iterator p = node->beginl_sc(), p_end = node->endl_sc(); p != p_end; ++p )
-		if ( DTR.addDataEntry ( p->bp(), p->getDep() ) )	// clash found
-			return true;
-	return false;
-}
-
-/// set RESULT into set of instances of A such that they do have data roles R and S
-void
-ReasoningKernel :: getDataRelatedIndividuals ( TDRoleExpr* R, TDRoleExpr* S, int op, IndividualSet& Result )
-{
-	preprocessKB();	// ensure KB is ready to answer the query
-	Result.clear();
-	const TRole* r = getRole ( R, "Role expression expected in the getIndividualsWith()" );
-	const TRole* s = getRole ( S, "Role expression expected in the getIndividualsWith()" );
-
-	// prepare DT reasoners
-	const DLDag& dag = getTBox()->getDag();
-	const DataTypeCenter& dtc = getTBox()->getDataTypeCenter();
-
-	DataTypeReasoner Op1(dag);
-	DataTypeReasoner Op2(dag);
-	dtc.initDataTypeReasoner(Op1);
-	dtc.initDataTypeReasoner(Op2);
-
-	// vector of individuals
-	typedef TDLNAryExpression<TDLIndividualExpression> IndVec;
-	IndVec Individuals ("individual expression","data related individuals");
-	Individuals.add(getExpressionManager()->getArgList());
-	for ( IndVec::iterator q = Individuals.begin(), q_end = Individuals.end(); q != q_end; ++q )
-	{
-		const TIndividual* ind = getIndividual ( *q, "individual name expected in getDataRelatedIndividuals()" );
-		const DlCompletionTree* vR = NULL;
-		const DlCompletionTree* vS = NULL;
-		for ( DlCompletionTree::const_edge_iterator p = ind->node->begin(), p_end = ind->node->end(); p != p_end; ++p )
-		{
-			const DlCompletionTreeArc* edge = *p;
-			if ( edge->isNeighbour(r) )
-				vR = edge->getArcEnd();
-			else if ( edge->isNeighbour(s) )
-				vS = edge->getArcEnd();
-			if ( vR && vS )
-			{
-				if ( fillDTReasoner ( Op1, vR ) )
-					break;
-				if ( fillDTReasoner ( Op2, vS ) )
-					break;
-				if ( checkDataRelation ( Op1, Op2, op ) )
-					Result.push_back(ind);
-				break;
-			}
-		}
-	}
 }
 
 //----------------------------------------------------------------------------------
