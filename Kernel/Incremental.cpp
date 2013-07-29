d6b53903c36b009c0ac007253230ef7d432bf456
dmitry.tsarkov <dmitry.tsarkov@2c28adb8-172f-0410-82af-bdfbc96ff48c>
2013-07-29 16:55:44 +0000

    Remove incrementral leftovers
    git-svn-id: https://factplusplus.googlecode.com/svn/trunk@1624 2c28adb8-172f-0410-82af-bdfbc96ff48c

--- Kernel/Incremental.cpp
+++ Kernel/Incremental.cpp
@@ -23,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "Actor.h"
 #include "tOntologyPrinterLISP.h"
 #include "procTimer.h"
-#include "IncrementalClassifier.h"
 
 TsProcTimer moduleTimer, subCheckTimer;
 int nModule = 0;
