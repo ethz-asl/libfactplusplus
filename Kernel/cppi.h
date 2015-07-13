5882890352c378bec77f25ef82b01a4248871ce4
Dmitry Tsarkov <dmitry.tsarkov@gmail.com>
2015-07-13 10:12:49 +0100

    [c++11]: minor cleanup

--- Kernel/cppi.h
+++ Kernel/cppi.h
@@ -1,5 +1,5 @@
 /* This file is part of the FaCT++ DL reasoner
-Copyright (C) 2003-2010 by Dmitry Tsarkov
+Copyright (C) 2003-2015 by Dmitry Tsarkov
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -32,7 +32,8 @@ class CPPI: public ProgressIndicatorInterface
 {
 protected:	// members
 		/// previous and current numbers shown
-	unsigned int oldPercent, curPercent;
+	unsigned int oldPercent = 0;
+	unsigned int curPercent = 0;
 
 protected:	// methods
 		/// initial exposition
@@ -54,14 +55,10 @@ public:		// interface
 		/// empty c'tor
 	CPPI ( void )
 		: ProgressIndicatorInterface()
-		, oldPercent(0)
-		, curPercent(0)
 		{}
 		/// init c'tor
 	CPPI ( unsigned long limit )
 		: ProgressIndicatorInterface(limit)
-		, oldPercent(0)
-		, curPercent(0)
 		{}
 		/// empty d'tor
 	virtual ~CPPI ( void ) {}
